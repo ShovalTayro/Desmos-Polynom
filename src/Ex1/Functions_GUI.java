3565f439e2d8d5d7ba1f66e4401d9383a2c8866e
uriyahav <uriyahavshush@gmail.com>
2019-11-28 18:38:21 +0200

    override many methods of Functions_GUI

--- src/Ex1/Functions_GUI.java
+++ src/Ex1/Functions_GUI.java
@@ -1,5 +1,109 @@
 package Ex1;
 
-public class Functions_GUI {
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Functions_GUI implements functions {
+	LinkedList<function> fun = new LinkedList<function>();
+	@Override
+	public boolean add(function arg0) {
+		boolean ans = fun.add(arg0);
+		return ans;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends function> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		fun.clear();
+	}
+
+	@Override
+	public boolean contains(Object arg0) {
+		boolean ans = fun.contains(arg0);
+		return ans;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		boolean ans = fun.isEmpty();
+		return ans;
+	}
+
+	@Override
+	public Iterator<function> iterator() {
+		return fun.iterator();
+	}
+
+	@Override
+	public boolean remove(Object arg0) {
+		boolean ans = fun.remove(arg0);
+		return ans;
+		}
+
+	@Override
+	public boolean removeAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		return fun.size();
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initFromFile(String file) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void saveToFile(String file) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawFunctions(int width, int height, Range rx, Range ry, int resolution) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawFunctions(String json_file) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
